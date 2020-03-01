6a55c5fe1210708ae57fd5f2d82265977ccf3f66
trangsinh87 <trangsinh87@yahoo.com.vn>
2020-03-02 00:59:48 +0700

    update

--- modules/admin/admin.route.js
+++ modules/admin/admin.route.js
@@ -1,9 +1,17 @@
 'use strict';
 
 module.exports = function(app) {
-	app.get('/admin', (req, res) => {
-		res.render('admin', {
-			layout: 'admin'
-		});
+	app.get('/admin', async (req, res) => {
+		try {
+			let userCount = await app.models.User.countDocuments({}),
+				clientCount = await app.models.Client.countDocuments({});
+			res.render('admin', {
+				layout: 'admin',
+				userCount: userCount,
+				clientCount: clientCount
+			});
+		} catch (err) {
+
+		}
 	});
 };
