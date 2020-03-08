afcaf08972dfc830e7512a27af18468ad6d2ff61
trangsinh87 <trangsinh87@yahoo.com.vn>
2020-03-08 21:27:55 +0700

    update

--- modules/www/operator/bo/backoffice.route.js
+++ modules/www/operator/bo/backoffice.route.js
@@ -7,4 +7,42 @@ module.exports = function(app) {
 			layout: 'admin'
 		});
 	});
+
+	app.get('/bo/statistics', async (req, res) => {
+		let client = await app.models.Client.aggregate([{
+			$group: {
+				_id: {
+					day: {
+						$dayOfMonth: "$created"
+					},
+					month: {
+						$month: "$created"
+					},
+					year: {
+						$year: "$created"
+					}
+				},
+				count: {
+					$sum: 1
+				},
+				date: {
+					$first: "$created"
+				}
+			}
+		}, {
+			$project: {
+				date: {
+					$dateToString: {
+						format: "%Y-%m-%d",
+						date: "$date"
+					}
+				},
+				count: 1,
+				_id: 0
+			}
+		}]);
+		console.log(client);
+		res.json(client);
+	});
+
 };
