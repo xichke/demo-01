21d68113ae7f0c2c41bcd8cf72c76007533b0b36
trangsinh87 <trangsinh87@yahoo.com.vn>
2020-03-09 23:01:24 +0700

    update

--- modules/www/operator/bo/client/bo.client.route.js
+++ modules/www/operator/bo/client/bo.client.route.js
@@ -1,9 +1,18 @@
 'use strict';
+const moment = require('moment');
 
 module.exports = function(app) {
-	app.get('/bo/client', (req, res) => {
+	app.get('/bo/client', async (req, res) => {
+		let clients = await app.models.Client.find({
+			operator: req.operator._id
+		}).lean();
+		clients.forEach(e => {
+			e.phone = app.utils.phone.mask(e.phone);
+			e.created = moment(e.created).fromNow()
+		});
 		res.render('bo/client', {
-			layout: 'backoffice'
+			layout: 'backoffice',
+			clients: clients
 		});
 	});
 };
